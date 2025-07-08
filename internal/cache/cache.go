784d50ca7ca1772766f423d8375aab9054735019
kmax17 <max@maxkaya.com>
2025-07-08 00:22:58 -0400

    init cache

---
+++ internal/cache/cache.go
@@ -0,0 +1,48 @@
+package cache
+
+import (
+	"github.com/google/go-github/v67/github"
+	"github.com/rs/zerolog/log"
+	"peanut/internal/config"
+	gh "peanut/internal/github"
+)
+
+var (
+	LatestRelease *github.RepositoryRelease
+	Releases      []*github.RepositoryRelease
+)
+
+func Init() error {
+	releases, _, err := gh.GHClient.Repositories.ListReleases(gh.GHCtx, config.Config.GHRepoOwner, config.Config.GHRepository, &github.ListOptions{PerPage: 100})
+	if err != nil {
+		log.Error().Err(err).Msg("Error getting releases")
+		return err
+	}
+
+	Releases = releases
+	LatestRelease = releases[0]
+
+	return nil
+}
+
+func Refresh() {
+	log.Info().Msg("Refreshing cache...")
+
+	err := Init()
+	if err != nil {
+		log.Error().Err(err).Msg("Error refreshing cache")
+		return
+	}
+
+	log.Info().Msg("Refreshed cache.")
+}
+
+func FindVersion(version string) (*github.RepositoryRelease, error) {
+	for _, release := range Releases {
+		if *release.TagName == version {
+			return release, nil
+		}
+	}
+
+	return nil, nil
+}
