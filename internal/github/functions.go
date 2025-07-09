7807bfdea909325fb355cc81463d3bf74ea1af2d
kmax17 <max@maxkaya.com>
2025-07-08 21:56:24 -0400

    refactor: adapt github client to multi repo config (#4)

--- internal/github/functions.go
+++ internal/github/functions.go
@@ -3,6 +3,7 @@ package github
 import (
 	"context"
 	"github.com/google/go-github/v67/github"
+	"github.com/rs/zerolog/log"
 	"peanut/internal/config"
 )
 
@@ -19,4 +20,14 @@ func Init() error {
 	}
 
 	return nil
+}
+
+func GetReleases(owner string, repo string) ([]*github.RepositoryRelease, error) {
+	releases, _, err := GHClient.Repositories.ListReleases(GHCtx, owner, repo, &github.ListOptions{PerPage: 100})
+	if err != nil {
+		log.Error().Err(err).Msg("Error getting releases")
+		return nil, err
+	}
+
+	return releases, nil
 }
