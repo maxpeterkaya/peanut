1e282ced6223c6914794fd186a92a27036c7ee08
maxpeterkaya <max@maxkaya.com>
2025-07-10 20:23:35 -0400

    feat: set commit to sha of current time (#12)

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -1,8 +1,19 @@
 package buildinfo
 
+import (
+	"github.com/maxpeterkaya/peanut/common"
+	"time"
+)
+
 var (
 	Version   = "dev"
 	Commit    = "none"
 	BuildTime = "unknown"
 	GoVersion = "unknown"
 )
+
+func init() {
+	if Version == "dev" {
+		Commit = common.SHAHash(time.Now().String())
+	}
+}
