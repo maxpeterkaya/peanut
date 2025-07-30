2c0f18917c56618e573ca2d102483e9b8a84a2d1
maxpeterkaya <max@maxkaya.com>
2025-07-29 21:07:05 -0400

    fix: change id types in init migration (#8)

--- db/migrations/000001_init.up.sql
+++ db/migrations/000001_init.up.sql
@@ -1,48 +1,49 @@
 CREATE TABLE repository
 (
-    id        uuid PRIMARY KEY,
-    owner     text,
-    name      text,
-    token     text,
-    isPrivate bool      default false,
-    createdAt timestamp default now(),
-    updatedAt timestamp default now()
+    id         serial PRIMARY KEY,
+    owner      text,
+    name       text,
+    token      text,
+    is_private bool      default false,
+    created_at timestamp default now(),
+    updated_at timestamp default now()
 );
 
-CREATE TABLE release (
-                         id int PRIMARY KEY,
-                         name text,
-                         tagName text,
-                         body text,
-                         draft bool default false,
-                         prerelease bool default false,
-                         createdAt timestamp,
-                         publishedAt timestamp,
-                         authorName text,
-                         authorId text,
-                         authorAvatarUrl text
+CREATE TABLE release
+(
+    id                int PRIMARY KEY,
+    name              text,
+    tag_name          text,
+    body              text,
+    is_draft          bool default false,
+    is_prerelease     bool default false,
+    created_at        timestamp,
+    published_at      timestamp,
+    author_name       text,
+    author_id         text,
+    author_avatar_url text
 );
 
 CREATE TABLE asset
 (
-    id            int,
-    apiURL        text,
-    url           text,
-    name          text,
-    contentLength int,
-    downloadCount int,
-    viewCount     int,
-    createdAt     timestamp,
-    updatedAt     timestamp,
-    uploadedAt    timestamp
+    id             int PRIMARY KEY,
+    api_url        text,
+    url            text,
+    name           text,
+    content_length int,
+    download_count int,
+    view_count     int,
+    created_at     timestamp,
+    updated_at     timestamp,
+    uploaded_at    timestamp
 );
 
 CREATE TABLE "user"
 (
-    id          uuid DEFAULT gen_random_uuid(),
-    username    VARCHAR(40),
-    displayName VARCHAR(40),
-    passHash    text,
-    createdAt   timestamp,
-    updatedAt   timestamp
+    id           serial PRIMARY KEY,
+    username     VARCHAR(40),
+    display_name VARCHAR(40),
+    pass_hash    text,
+    created_at   timestamp default now(),
+    updated_at   timestamp default now()
 );
