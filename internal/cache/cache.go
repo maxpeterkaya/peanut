2e2f2e58a9c344b160a5a84a2984bc2f53094efd
maxpeterkaya <max@maxkaya.com>
2025-08-17 23:00:09 -0400

    feat: add abstraction layer to cache for enabled database (#8)

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -2,11 +2,17 @@ package cache
 
 import (
 	"errors"
-	"github.com/google/go-github/v67/github"
-	"github.com/rs/zerolog/log"
+	"fmt"
 	"peanut/internal/config"
+	"peanut/internal/database"
 	gh "peanut/internal/github"
+	"peanut/internal/repository"
+	"peanut/internal/services"
 	"slices"
+	"strconv"
+
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/rs/zerolog/log"
 )
 
 var (
@@ -17,11 +23,46 @@ type Repo struct {
 	Owner         string
 	Token         string
 	Name          string
-	LatestRelease *github.RepositoryRelease
-	Releases      []*github.RepositoryRelease
+	LatestRelease *repository.Release
+	Releases      []repository.Release
+	Assets        []repository.Asset
 }
 
 func Init() error {
+	if config.Config.Common.EnableDatabase {
+		repos, err := database.Queries.ListRepositories(database.CTX)
+		if err != nil {
+			log.Fatal().Err(err).Msg("failed to get repositories to cache")
+			return err
+		}
+
+		for _, repo := range repos {
+			fullRepo := fmt.Sprintf("%v/%v", repo.Owner.String, repo.Name.String)
+
+			releases, err := gh.GetReleases(repo.Owner.String, repo.Name.String)
+			if err != nil {
+				log.Error().Err(err).Str("repo", fullRepo).Msg("Failed to get releases")
+				return err
+			}
+
+			for _, release := range releases {
+				createRelease, err := services.CreateRelease(*release, fullRepo, repo)
+				if err != nil {
+					return err
+				}
+
+				for _, asset := range release.Assets {
+					_, err := services.CreateAsset(*asset, fullRepo, *createRelease)
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+
+		return nil
+	}
+
 	for _, repo := range config.Config.Github.Repositories {
 		releases, err := gh.GetReleases(config.Config.Github.Owner, repo)
 		if err != nil {
@@ -29,16 +70,31 @@ func Init() error {
 			return err
 		}
 
+		authorName := fmt.Sprintf("%v", releases[0].Author.Name)
+
+		data := Repo{
+			Owner: config.Config.Github.Owner,
+			Token: config.Config.Github.Token,
+			Name:  repo,
+			LatestRelease: &repository.Release{
+				ID:              int32(*releases[0].ID),
+				GithubID:        int32(*releases[0].ID),
+				Name:            pgtype.Text{Valid: true, String: *releases[0].TagName},
+				Body:            pgtype.Text{Valid: true, String: *releases[0].Body},
+				IsDraft:         pgtype.Bool{Valid: true, Bool: *releases[0].Draft},
+				IsPrerelease:    pgtype.Bool{Valid: true, Bool: *releases[0].Prerelease},
+				PublishedAt:     pgtype.Timestamp{Time: releases[0].PublishedAt.Time},
+				AuthorName:      pgtype.Text{Valid: true, String: authorName},
+				AuthorID:        pgtype.Text{Valid: true, String: strconv.FormatInt(*releases[0].Author.ID, 10)},
+				AuthorAvatarUrl: pgtype.Text{Valid: true, String: *releases[0].Author.AvatarURL},
+				RepositoryID:    pgtype.Int4{Valid: true, Int32: int32(0)},
+			},
+			Releases: services.GithubReleasesToRepoReleases(releases),
+			Assets:   services.GithubAssetsToRepoAssets(releases),
+		}
+
 		if len(Repositories) == 0 {
-			Repositories = []Repo{
-				{
-					Owner:         config.Config.Github.Owner,
-					Token:         config.Config.Github.Token,
-					Name:          repo,
-					LatestRelease: releases[0],
-					Releases:      releases,
-				},
-			}
+			Repositories = []Repo{data}
 		}
 
 		index := slices.IndexFunc(Repositories, func(r Repo) bool {
@@ -46,21 +102,9 @@ func Init() error {
 		})
 
 		if index == -1 {
-			Repositories = append(Repositories, Repo{
-				Owner:         config.Config.Github.Owner,
-				Token:         config.Config.Github.Token,
-				Name:          repo,
-				LatestRelease: releases[0],
-				Releases:      releases,
-			})
+			Repositories = append(Repositories, data)
 		} else {
-			Repositories[index] = Repo{
-				Owner:         config.Config.Github.Owner,
-				Token:         config.Config.Github.Token,
-				Name:          repo,
-				LatestRelease: releases[0],
-				Releases:      releases,
-			}
+			Repositories[index] = data
 		}
 	}
 
@@ -79,12 +123,31 @@ func Refresh() {
 	log.Info().Msg("Refreshed cache.")
 }
 
-func FindVersion(repo string, version string) (*github.RepositoryRelease, error) {
+func FindVersion(repo string, version string) (*repository.Release, error) {
+	if config.Config.Common.EnableDatabase {
+		rp, err := services.SearchRepo(repo)
+		if err != nil {
+			log.Error().Err(err).Str("repo", repo).Str("version", version).Msg("Failed to search repo")
+			return nil, err
+		}
+
+		release, err := database.Queries.GetReleaseVersion(database.CTX, repository.GetReleaseVersionParams{
+			TagName:      pgtype.Text{Valid: true, String: version},
+			RepositoryID: pgtype.Int4{Valid: true, Int32: rp.ID},
+		})
+		if err != nil {
+			log.Error().Err(err).Str("repo", repo).Str("version", version).Msg("Failed to get release version")
+			return nil, err
+		}
+
+		return &release, nil
+	}
+
 	for _, r := range Repositories {
 		if r.Name == repo {
 			for _, v := range r.Releases {
-				if *v.Name == version {
-					return v, nil
+				if v.TagName.String == version {
+					return &v, nil
 				}
 			}
 		}
@@ -93,7 +156,22 @@ func FindVersion(repo string, version string) (*github.RepositoryRelease, error)
 	return nil, nil
 }
 
-func FindLatest(repo string) (*github.RepositoryRelease, error) {
+func FindLatest(repo string) (*repository.Release, error) {
+	if config.Config.Common.EnableDatabase {
+		rp, err := services.SearchRepo(repo)
+		if err != nil {
+			log.Error().Err(err).Str("repo", repo).Str("version", "latest").Msg("Failed to search repo")
+			return nil, err
+		}
+
+		release, err := database.Queries.GetLatestRelease(database.CTX, pgtype.Int4{Valid: true, Int32: rp.ID})
+		if err != nil {
+			log.Error().Err(err).Str("repo", repo).Str("version", "latest").Msg("Failed to get latest release")
+		}
+
+		return &release, nil
+	}
+
 	for _, r := range Repositories {
 		if r.Name == repo {
 			return r.LatestRelease, nil
@@ -103,7 +181,23 @@ func FindLatest(repo string) (*github.RepositoryRelease, error) {
 	return nil, errors.New("no releases found")
 }
 
-func FindReleases(repo string) ([]*github.RepositoryRelease, error) {
+func FindReleases(repo string) ([]repository.Release, error) {
+	if config.Config.Common.EnableDatabase {
+		rp, err := services.SearchRepo(repo)
+		if err != nil {
+			log.Error().Err(err).Str("repo", repo).Msg("Failed to search repo")
+			return nil, err
+		}
+
+		releases, err := services.ListReleases(rp.ID)
+		if err != nil {
+			log.Error().Err(err).Str("repo", repo).Msg("Failed to list releases")
+			return nil, err
+		}
+
+		return releases, nil
+	}
+
 	for _, r := range Repositories {
 		if r.Name == repo {
 			return r.Releases, nil
@@ -111,4 +205,32 @@ func FindReleases(repo string) ([]*github.RepositoryRelease, error) {
 	}
 
 	return nil, errors.New("no releases found")
+}
+
+func FindAssets(releaseID int32) ([]repository.Asset, error) {
+	if config.Config.Common.EnableDatabase {
+		assets, err := services.GetAssets(releaseID)
+		if err != nil {
+			log.Trace().Err(err).Int("releaseID", int(releaseID)).Msg("Failed to get assets")
+			return nil, err
+		}
+
+		return assets, nil
+	}
+
+	array := make([]repository.Asset, 0)
+
+	for _, r := range Repositories {
+		for _, asset := range r.Assets {
+			if asset.ReleaseID.Int32 == releaseID {
+				array = append(array, asset)
+			}
+		}
+	}
+
+	if len(array) == 0 {
+		return nil, errors.New("no assets found")
+	}
+
+	return array, nil
 }
