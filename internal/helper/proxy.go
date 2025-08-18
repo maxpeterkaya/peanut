eb71e7db7bca7414c7eb98cb0490d9b71ac62934
maxpeterkaya <max@maxkaya.com>
2025-08-17 22:59:29 -0400

    feat: adjust download proxy to use database models (#8)

--- internal/helper/proxy.go
+++ internal/helper/proxy.go
@@ -2,25 +2,25 @@ package helper
 
 import (
 	"fmt"
-	"github.com/google/go-github/v67/github"
-	"github.com/rs/zerolog/log"
 	"io"
 	"net/http"
-	"peanut/internal/config"
+	"peanut/internal/repository"
 	"strconv"
 	"time"
+
+	"github.com/rs/zerolog/log"
 )
 
-func ProxyDownload(asset github.ReleaseAsset, token string, w http.ResponseWriter, r *http.Request) {
-	url := *asset.URL
+func ProxyDownload(repo repository.Repository, asset repository.Asset, w http.ResponseWriter, r *http.Request) {
+	url := asset.ApiUrl.String
 	client := &http.Client{
 		Timeout: time.Second * 100,
 	}
 
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("Accept", "application/octet-stream")
-	if len(config.Config.Github.Token) > 0 {
-		req.Header.Set("Authorization", "Bearer "+token)
+	if len(repo.Token.String) > 0 {
+		req.Header.Set("Authorization", "Bearer "+repo.Token.String)
 	}
 
 	resp, err := client.Do(req)
@@ -30,11 +30,11 @@ func ProxyDownload(asset github.ReleaseAsset, token string, w http.ResponseWrite
 	}
 	defer resp.Body.Close()
 
-	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, *asset.Name))
+	w.Header().Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, asset.Name.String))
 	w.Header().Set("Content-Length", strconv.FormatInt(resp.ContentLength, 10))
 	_, err = io.Copy(w, resp.Body)
 	if err != nil {
-		log.Error().Err(err).Msgf("Error copying %s", url)
+		log.Error().Err(err).Msgf("Error copying %s/%s/%s", repo.Owner.String, repo.Name.String, asset.Name.String)
 		return
 	}
 }
